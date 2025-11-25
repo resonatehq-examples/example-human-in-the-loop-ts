48f63c51c4b9d5530a5d0758b3ba7c6d0e96fffe
Cully Wakelin <cully.wakelin@resonatehq.io>
2025-11-25 16:31:37 -0500

    update to use latest version of sdk

--- worker.ts
+++ worker.ts
@@ -1,12 +1,12 @@
 import { Resonate } from "@resonatehq/sdk";
 import type { Context } from "@resonatehq/sdk";
 
-const resonate = Resonate.remote({
+const resonate = new Resonate({
+  url: "http://localhost:8001",
   group: "workers",
 });
 
-function sendEmail(ctx: Context, promiseId: string) {
-  console.log(`Simulating sending email for promise ${promiseId}`);
+async function sendEmail(_: Context, promiseId: string) {
   // Simulate sending an email to the user with a link to resolve the promise
   console.log(
     `Email sent! Please resolve the promise by visiting: http://localhost:5001/unblock-workflow?promise_id=${promiseId}`
@@ -14,23 +14,18 @@ function sendEmail(ctx: Context, promiseId: string) {
   return `Email sent for promise ${promiseId}`;
 }
 
-function* foo(ctx: Context, workflowId: string) {
-  console.log(`foo workflow ${workflowId} started`);
-
+function* fooWorkflow(ctx: Context, workflowId: string) {
   const blockingPromise = yield* ctx.promise({});
   yield* ctx.run(sendEmail, blockingPromise.id);
-
+  console.log(`workflow blocked, waiting on human interaction`);
+  // Wait for the promise to be resolved
+  const data = yield* blockingPromise;
   console.log(
-    `foo workflow ${workflowId} blocked, waiting on human interaction`
+    `foo workflow ${workflowId} unblocked, promise resolved with ${data}`
   );
-
-  yield* blockingPromise;
-
-  console.log(`foo workflow ${workflowId} unblocked, promise resolved`);
-
   return `foo workflow ${workflowId} complete`;
 }
 
-resonate.register("foo", foo);
+resonate.register("foo-workflow", fooWorkflow);
 
 console.log("worker running");
